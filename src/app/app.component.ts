e5a46163c0c7c46cf05b621bf77cc18684ea0cd2
DanielRochaRmrz <89757376+DanielRochaRmrz@users.noreply.github.com>
2025-02-01 22:25:19 -0600

    Eliminar manifest.webmanifest e implementar la solicitud de instalación en app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import Swal from 'sweetalert2';
 @Component({
     selector: 'app-root',
     imports: [RouterModule, RouterOutlet, MatCardModule, MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule],
@@ -16,6 +17,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 export class AppComponent {
   private router = inject(Router);
   public title = '';
+  protected deferredPrompt: any;
   constructor() {
     this.router.events.subscribe((router: any) => {
       switch (router.url) {
@@ -37,4 +39,38 @@ export class AppComponent {
       }
     });
   }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event) {
+    event.preventDefault();
+    this.deferredPrompt = event;
+
+    // Esperar 5 segundos antes de mostrar la alerta
+    setTimeout(() => {
+      this.showInstallAlert();
+    }, 5000);
+  }
+
+  showInstallAlert() {
+    Swal.fire({
+      title: 'Instalar App',
+      text: '¿Deseas instalar la aplicación?',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.deferredPrompt.prompt();
+        this.deferredPrompt.userChoice.then((choiceResult: any) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('Usuario aceptó la instalación');
+          } else {
+            console.log('Usuario canceló la instalación');
+          }
+        });
+      }
+    });
+  }
+
 }
